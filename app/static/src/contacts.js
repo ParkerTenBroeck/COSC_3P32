72a40f54d9ff90a5f987d679e71143e0191a82ed
Parker TenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2024-04-26 15:20:00 -0400

    added rest of context api to javascript

--- app/static/src/contacts.js
+++ app/static/src/contacts.js
@@ -13,6 +13,57 @@ export async function add_contact(contact_id) {
     }
 }
 
+export async function find_user_email(email){
+    const resp = await fetch("/database/find_user_email", {
+        credentials: "same-origin",
+        mode: "same-origin",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: email
+    });
+
+    if (!resp.ok) {
+        console.log("Status: " + resp.status)
+        return Promise.reject("server")
+    }
+
+    return await resp.json();
+}
+
+export async function find_user_username(email){
+    const resp = await fetch("/database/find_user_username", {
+        credentials: "same-origin",
+        mode: "same-origin",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: email
+    });
+
+    if (!resp.ok) {
+        console.log("Status: " + resp.status)
+        return Promise.reject("server")
+    }
+
+    return await resp.json();
+}
+
+export async function find_user_phone(email){
+    const resp = await fetch("/database/find_user_phone", {
+        credentials: "same-origin",
+        mode: "same-origin",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: email
+    });
+
+    if (!resp.ok) {
+        console.log("Status: " + resp.status)
+        return Promise.reject("server")
+    }
+
+    return await resp.json();
+}
+
 export async function delete_contact(contact_id){
     const resp = await fetch("/database/delete_contact", {
         credentials: "same-origin",
